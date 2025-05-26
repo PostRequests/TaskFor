9f7509f2757078067f22bb2034f3399c84ee0687
Borodin <servikb1@gmail.com>
2025-05-26 11:30:02 +0700

    Задание 14.4

--- Task.h
+++ Task.h
@@ -1,11 +1,14 @@
 #pragma once
 #include "MyClass/Menu/Menu.h"
+#include <algorithm>
+#include <set>
 class Task
 {
 private:
+	
 public:
 	void run(); 
-
+	Task() {}
 	//вспомогательные методы
 
 	/// <summary>
@@ -13,6 +16,16 @@ public:
 	/// </summary>
 	/// <param name="v">Одномерный вектор</param>
 	 template <typename T>
-	 static std::string toStr(std::vector<T>& v);
+	 static std::string toStr(const std::vector<T>& v);
+	 /// <summary>
+	/// Переводит set в строку 
+	/// </summary>
+	/// <param name="v">Одномерный вектор</param>
+	template <typename T>
+	 static std::string toStr(const std::set<T>& v);
+	 /**
+	 * @brief Заполняет вектор случайными числами от min до max размером size
+	 */
+	 static std::vector<int> randVec( int min, int max, int size);
 };
 
